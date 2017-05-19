87961f4c11c5f4e364ca13c70f75b4ed0f9a7a8d
azogue <eugenio.panadero@gmail.com>
2017-05-19 14:26:36 +0200

    Handle pusbullet errors

--- enerpi/notifier/__init__.py
+++ enerpi/notifier/__init__.py
@@ -15,7 +15,7 @@ Pushbullet API config:
 from configparser import NoOptionError, NoSectionError
 import datetime as dt
 from pushbullet import Pushbullet
-from pushbullet.errors import InvalidKeyError
+from pushbullet.errors import InvalidKeyError, PushError, PushbulletError
 from enerpi.base import CONFIG, log, async_task, RECIPIENT
 
 
@@ -34,7 +34,8 @@ def _get_pb_obj():
                 raise InvalidKeyError
             PBOBJ = Pushbullet(pushbullet_token)
         except (NoOptionError, NoSectionError, InvalidKeyError) as e:
-            log('NO Pushbullet config ({} [{}])'.format(e, e.__class__), 'error', False)
+            log('NO Pushbullet config ({} [{}])'
+                .format(e, e.__class__), 'error', False)
             PBOBJ = None
     return PBOBJ
 
@@ -42,30 +43,40 @@ def _get_pb_obj():
 def _send_direct_push_notification(title, content, email=RECIPIENT):
     pb = _get_pb_obj()
     if (pb is not None) and email:
-        pb.push_note(title, content, email=email)
-        log('PUSHBULLET NOTIFICATION: {} - {}'.format(title, content), 'debug', False)
-        return True
-    log('NO PUSHBULLET NOTIFICATION (title={}, email={})'.format(title, email),
-        'error', True)
+        try:
+            pb.push_note(title, content, email=email)
+            log('PUSHBULLET NOTIFICATION: {} - {}'
+                .format(title, content), 'debug', False)
+            return True
+        except (PushError, PushbulletError) as exc:
+            log('{} ERROR: {}'
+                .format(exc.__class__, exc), 'error', True)
+    log('NO PUSHBULLET NOTIFICATION (title={}, email={})'
+        .format(title, email), 'error', True)
     return False
 
 
 @async_task
-def push_enerpi_error(title_err, msg_error):
+def push_enerpi_error(title_err, msg_error, email=RECIPIENT):
     """
-    Send an error message notification (vía pushbullet API) to the default recipient.
+    Send an error message notification.
+
+    Vía pushbullet API, to the default recipient.
     :param title_err: error title
     :param msg_error: error text content
+    :param email: target email
 
     """
-    return _send_direct_push_notification('ERROR: {}\n(time:{:%H:%M:%S})'.format(title_err, dt.datetime.now()),
-                                          msg_error, email=RECIPIENT)
+    return _send_direct_push_notification(
+        'ERROR: {}\n(time:{:%H:%M:%S})'.format(
+            title_err, dt.datetime.now()), msg_error, email=email)
 
 
 @async_task
 def push_enerpi_channel_msg(title, msg, channel_tag=CHANNEL):
     """
-    Send a text notification (vía pushbullet API) to the ENERPI pushbullet channel (timestamped)
+    Send a text notification (vía pushbullet API)
+     to the ENERPI pushbullet channel (timestamped)
 
     :param title:
     :param msg:
@@ -76,13 +87,19 @@ def push_enerpi_channel_msg(title, msg, channel_tag=CHANNEL):
     pb = _get_pb_obj()
     if pb is not None:
         try:
-            ch = list(filter(lambda x: x.channel_tag == channel_tag, pb.channels))[0]
+            ch = list(filter(
+                lambda x: x.channel_tag == channel_tag, pb.channels))[0]
         except IndexError:
             ch = None
         if ch:
-            ch.push_note(title, content)
-            log('PUSHBULLET CHANNEL NOTIFICATION: {} - {}'.format(title, content), 'debug', False)
-            return True
+            try:
+                ch.push_note(title, content)
+                log('PUSHBULLET CHANNEL NOTIFICATION: {} - {}'
+                    .format(title, content), 'debug', False)
+                return True
+            except (PushError, PushbulletError) as exc:
+                log('{} ERROR: {}'
+                    .format(exc.__class__, exc), 'error', True)
     log('NO PUSHBULLET CHANNEL NOTIFICATION (title={}, msg={}, channel={})'
         .format(title, msg, channel_tag), 'error', True)
     return False
